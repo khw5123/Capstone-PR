d185d766396439fbd9108e0e6a18e6a5332ab2aa
kimkc <gc5215@nate.com>
2020-08-29 22:12:51 +0900

    refatoring xmlparsing in working plan page

--- src/main/java/com/capstone/farming/controller/FarmWorkingPlanController.java
+++ src/main/java/com/capstone/farming/controller/FarmWorkingPlanController.java
@@ -1,8 +1,10 @@
 package com.capstone.farming.controller;
 
 import com.capstone.farming.model.WorkingPlan;
+import com.capstone.farming.service.XMLParseService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,106 +26,30 @@ import java.util.HashMap;
 public class FarmWorkingPlanController {
     private final String apiKey = "20200519ARN2H1BVVDHK2Y5BBHDPZG"; // API Key
 
-    @GetMapping("/plan")
-    public String getPlan(Model model, String kidofcomdtySeCode, String cntntsNo, String cntntsNoName) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
-        //수정필요
-        String url = "";
-        try {
-            // XML Parsing
-            dbFactoty = DocumentBuilderFactory.newInstance();
-            dBuilder = dbFactoty.newDocumentBuilder();
-            //수정필요
-            url = "http://api.nongsaro.go.kr/service/farmWorkingPlanNew/workScheduleGrpList?apiKey="+apiKey; // 요청 URL
-            Document doc = dBuilder.parse(url);
-            doc.getDocumentElement().normalize();
-            doc.getDocumentElement().getNodeName();
+    @Autowired
+    private XMLParseService xmlParseService;
 
-            HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-            NodeList items = doc.getElementsByTagName("item");
-            for(int i = 0; i < items.getLength(); i++) {
-                Node node = items.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) { // 해당 노드의 종류 판정(Element일 때)
-                    Element ele = (Element) node;
-                    hashMap.put(getTagValue("codeNm", ele), Integer.parseInt(getTagValue("kidofcomdtySeCode", ele)));
-                }
-            }
-            String json = new ObjectMapper().writeValueAsString(hashMap);
+    @GetMapping("/plan")
+    public String getPlan(Model model, String kidofcomdtySeCode, String cntntsNo, String cntntsNoName){
             model.addAttribute("selectedCode",kidofcomdtySeCode);
-            model.addAttribute("nameNcode", json);
-        } catch(Exception e) {
-            log.error("An error occurred in the FarmWorkingPlan method", e);
-        }
+            model.addAttribute("nameNcode", xmlParseService.getAreaCode());
 
         if (kidofcomdtySeCode != null && kidofcomdtySeCode != "" && (cntntsNo == null || cntntsNo == "")){
-            try {
-                dbFactoty = DocumentBuilderFactory.newInstance();
-                dBuilder = dbFactoty.newDocumentBuilder();
-                url = "http://api.nongsaro.go.kr/service/farmWorkingPlanNew/workScheduleLst?apiKey="+apiKey+"&kidofcomdtySeCode="+Integer.parseInt(kidofcomdtySeCode); // 요청 URL
-                Document doc = dBuilder.parse(url);
-                doc.getDocumentElement().normalize();
-                doc.getDocumentElement().getNodeName();
 
-                ArrayList<WorkingPlan> workingPlans = new ArrayList<>();
-                NodeList items = doc.getElementsByTagName("item");
-                for(int i = 0; i < items.getLength(); i++) {
-                    Node node = items.item(i);
-                    if (node.getNodeType() == Node.ELEMENT_NODE) { // 해당 노드의 종류 판정(Element일 때)
-                        Element ele = (Element) node;
-                        workingPlans.add(new WorkingPlan(Integer.parseInt(getTagValue("cntntsNo", ele)),
-                                getTagValue("fileDownUrlInfo", ele),
-                                getTagValue("fileName", ele)));
-                    }
-                }
-                model.addAttribute("selectedCode",kidofcomdtySeCode);
-                model.addAttribute("workingPlans",workingPlans);
-            } catch(Exception e) {
-                log.error("An error occurred in the FarmWorkingPlan method", e);
-            }
+            model.addAttribute("selectedCode",kidofcomdtySeCode);
+            model.addAttribute("workingPlans", xmlParseService.getPlanCrops(kidofcomdtySeCode));
+
         }
 
         if (cntntsNo != null && cntntsNo != ""){
-            try {
-                dbFactoty = DocumentBuilderFactory.newInstance();
-                dBuilder = dbFactoty.newDocumentBuilder();
-                url = "http://api.nongsaro.go.kr/service/farmWorkingPlanNew/workScheduleEraInfoLst?apiKey="+apiKey+"&cntntsNo="+Integer.parseInt(cntntsNo); // 요청 URL
-
-                Document doc = dBuilder.parse(url);
-                doc.getDocumentElement().normalize();
-                doc.getDocumentElement().getNodeName();
-
-                String htmlCn = "";
-                NodeList item = doc.getElementsByTagName("htmlCn");
-                for(int i = 0; i < item.getLength(); i++) {
-                    Node node = item.item(i);
-                    if (node.getNodeType() == Node.ELEMENT_NODE) { // 해당 노드의 종류 판정(Element일 때)
-                        Element ele = (Element) node;
-                        String nodeName = ele.getNodeName();
-                        htmlCn = ele.getTextContent();
-                    }
-                }
-
                 model.addAttribute("selectedCode", kidofcomdtySeCode);
                 if (cntntsNoName != null && cntntsNoName != ""){
                     model.addAttribute("cntntsNoName", cntntsNoName);
                 }
-                model.addAttribute("htmlCn", htmlCn);
-            } catch(Exception e) {
-                log.error("An error occurred in the FarmWorkingPlan method", e);
-            }
+                model.addAttribute("htmlCn", xmlParseService.getPlanHTML(cntntsNo));
         }
 
         return "WorkingPlan/plan";
-    }
-
-    private static String getTagValue(String tag, Element eElement) {
-        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
-        Node nValue = (Node) nlList.item(0);
-        if(nValue == null) {
-            return null;
-        }
-        return nValue.getNodeValue();
     }
 
 }
